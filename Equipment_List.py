f878eedea7fbe5249ee0302d9d08b92504e3b9fc
jinjiachen <jinjiachen_tux@163.com>
2019-06-01 22:29:32 +0800

    add small pro
    GSO and Equp Lst

--- Equipment_List.py
+++ Equipment_List.py
@@ -1,10 +1,18 @@
 #!/bin/python
 import os
 
+Version="Name: EC-search V0.1"
+Description="Description: This software is used to search the equipment lists quickly.The database will be synchronized with the lists."
 while True:
-    para=raw_input("Please input the EC number:")
-    cmd='find "'+para+'" .\Database\Equipment_List.db'
-#    print cmd
-    os.system(cmd)
-    input("Press Enter to exit! Others to continue!")
-    os.system("cls")
+    try:
+        para=raw_input("Please input the EC number:")
+        cmd='find "'+para+'" .\Database\Equipment_List.db'
+        os.system(cmd)
+        flag=raw_input("Press Enter to exit! Others to continue!")
+        if flag!="":
+            break
+        else:
+            os.system("cls")
+    except:
+        print("\nWarning: An error occurs! Please contact author.")
+        break
--- GSO_standards.py
+++ GSO_standards.py
@@ -1,10 +1,23 @@
 #!/bin/python
 import os
 
+Version="Name: G-search V0.1"
+Description="Description: This software is used to searh the GSO standards.The database will be updated as soon as new standards publish."
+line="-------------------------------------"
 while True:
-    para=raw_input("Please input the standard:")
-    cmd='find "'+para+'" .\Database\GSO.db'
-#    print cmd
-    os.system(cmd)
-    input("Press Enter to exit! Others to continue!")
-    os.system("cls")
+    print(line+"INFO"+line)
+    print(Version)
+    print(Description)
+    print(line+"----"+line)
+    try:
+        para=raw_input("Please input the standard:")
+        cmd='find "'+para+'" .\Database\GSO.db'
+        test=os.system(cmd)
+        flag=raw_input("Press Enter to continue! Others to EXIT!")
+        if flag!="":
+            break
+        else:
+            os.system("cls")
+    except:
+        print("\nWarning: There's an error! Please contact author.")
+        break
