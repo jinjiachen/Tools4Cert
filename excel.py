ea2ddd5f7b6716a2cae2619e3e9499afb88b55a6
jinjiachen <jinjiachen_tux@163.com>
2022-07-06 15:04:23 +0800

    revise 12.0

--- excel.py
+++ excel.py
@@ -28,9 +28,10 @@ def Menu():
         app=xw.App(visible=False,add_book=False)
         wb=app.books.open(rpt)
         sh=wb.sheets['4.0 Components']
+        sh12=wb.sheets['12.0 Revisions']
         wb1=app.books.open(data)
         sh1=wb1.sheets['4.0 Components']
-        update4(sh,sh1)
+        update4(sh,sh1,sh12)
 #        a=get_row_number(sh1,'h','A')
 #        data=copy_line(sh1,a)
 #        print(data)
@@ -257,13 +258,28 @@ def row_range(sheet,data): #xlwings:查找相同name的部件的行数范围
         if len(rows)==2:
             break
     return rows
-#    return min(rows),max(rows)
 
-def update4(sheet1,sheet2,):#xlwings:更新4.0信息
+#def row_range(sheet,col,words): #xlwings:查找相同name的部件的行数范围
+#    rows=[]
+#    for i in range(1,200):#在报告的此行数范围内去匹配
+#        if sheet[f'col{i}'].value==words:#c列中寻找关键词
+#            row_start=i #找到起始行
+#            rows.append(row_start)#找到对应的关键词，记录开始行
+#            row_end=lookdown(sheet,col,i)
+#            rows.append(row_end)#记录暂定的结束行，如果下方是同一部件，则会被后面的替代，如果不是，这就是最终的行数
+#            while(sheet[f'col{row_end+1}'].value==words):
+#                row_end=row_end+1#同name的部件的结束行
+#                rows[1]=row_end #找到同样的部件名，更新结束行
+#        if len(rows)==2:
+#            break
+#    return rows
+
+def update4(sheet1,sheet2,sheet3):#xlwings:更新4.0信息
+    row_rev=6
     for i in range(1,200): #在此行数范围内去匹配需要修改的信息
         if sheet2[f'h{i}'].value=="A": #判断H列是否为A，A为新增
             data=copy_line(sheet2,i)#复制对应行的数据
-            print('add:',data)
+#            print('add:',data)
             for j in range(1,200):#在报告的此行数范围内去匹配
                 if sheet1[f'c{j}'].value==data[1]:#c列中寻找data[1]，即Name
                     row=lookdown(sheet1,'c',j)
@@ -272,21 +288,30 @@ def update4(sheet1,sheet2,):#xlwings:更新4.0信息
                     print(row)
                     break
             insert_line(sheet1,row,list_fmt(data)) #在该行后面插入数据
+            data_rpt=data
+            update12(sheet3,row_rev,data_rpt,data,'A')
+#            insert_line(sheet3,row_rev,update12(data_rpt,data,'A'))
+#            sheet3[f'e{row_rev}'].value=update12(data_rpt,data,'A')
+            row_rev=row_rev+1
         elif sheet2[f'h{i}'].value=="RF": #判断H列是否为RF，RF为修改技术参数
             data=copy_line(sheet2,i)#复制对应行的数据
-            print('revise:',data)
+#            print('revise:',data)
+#            rows=row_range(sheet1,'c',data[1]) #返回对应部件相应的行数范围
             rows=row_range(sheet1,data) #返回对应部件相应的行数范围
-            print(rows)
+#            print(rows)
             for j in range(rows[0],rows[1]+1):#在同一个部件的行数范围内去匹配信息
                 data_rpt=copy_line(sheet1,j)
 #                if sheet1[f'd{j}'].value.upper()==data[2].upper() and sheet1[f'e{j}'].value.upper()==data[3].upper(): #匹配制造商与型号，当一致时，进行后面的操作
                 if data_rpt[2].upper()==data[2].upper() and data_rpt[3].upper()==data[3].upper(): #匹配制造商与型号，当一致时，进行后面的操作
                     data=list_fmt(data)
                     paste_line(sheet1,j,data) #修改技术参数(technical data), 用了整行复制的方法，但是其实只是修改技术参数那一列，因为部件名称，制造商，型号都是一致的
-                    print('revisie technical data:',data[4])
+#                    print('revisie technical data from',data_rpt[4],'to',data[4])
+                    update12(sheet3,row_rev,data_rpt,data,'RF')
+                    row_rev=row_rev+1
         elif sheet2[f'h{i}'].value=="RE": #判断H列是否为RE，RE为修改型号
             data=copy_line(sheet2,i)#复制对应行的数据
-            print('revise:',data)
+#            print('revise:',data)
+#            rows=row_range(sheet1,'c',data[1]) #返回对应部件相应的行数范围
             rows=row_range(sheet1,data) #返回对应部件相应的行数范围
             print(rows)
             for j in range(rows[0],rows[1]+1):#在同一个部件的行数范围内去匹配信息
@@ -295,12 +320,15 @@ def update4(sheet1,sheet2,):#xlwings:更新4.0信息
 #                if sheet1[f'd{j}'].value==data[1] and sheet1[f'f{j}'].value==data[3]: #匹配制造商与技术参数，当一致时，进行后面的操作
                     data=list_fmt(data)
                     paste_line(sheet1,j,data) #修改型号(model), 用了整行复制的方法，但是其实只是修改型号那一列，因为部件名称，制造商，技术参数都是一致的
-                    print('revise model:',data[3])
+#                    print('revise model:',data[3])
+                    update12(sheet3,row_rev,data_rpt,data,'RE')
+                    row_rev=row_rev+1
         elif sheet2[f'h{i}'].value=="RD": #判断H列是否为RD，RD为修改制造商
             data=copy_line(sheet2,i)#复制对应行的数据
-            print('revise:',data)
+#            print('revise:',data)
+#            rows=row_range(sheet1,'c',data[1]) #返回对应部件相应的行数范围
             rows=row_range(sheet1,data) #返回对应部件相应的行数范围
-            print(rows)
+#            print(rows)
             for j in range(rows[0],rows[1]+1):#在同一个部件的行数范围内去匹配信息
                 data_rpt=copy_line(sheet1,j)
 #                print(sheet1[f'e{j}'].value==data[2])
@@ -309,7 +337,22 @@ def update4(sheet1,sheet2,):#xlwings:更新4.0信息
                 if sheet1[f'e{j}'].value==data[3] and data_rpt[4]==data[4]: #匹配型号与技术参数，当一致时，进行后面的操作
 #                if sheet1[f'e{j}'].value==data[2] and sheet1[f'f{j}'].value==data[3]: #匹配型号与技术参数，当一致时，进行后面的操作
                     paste_line(sheet1,j,data) #修改制造商(manufacturer), 用了整行复制的方法，但是其实只是修改制造商那一列，因为部件名称，型号，技术参数都是一致的
-                    print('revise manufacturer:',data[2])
+#                    print('revise manufacturer:',data[2])
+                    update12(sheet3,row_rev,data_rpt,data,'RD')
+                    row_rev=row_rev+1
+
+def update12(sheet12,row,data_rpt,data,cmd):
+    if cmd=="RD":
+        sentence="Revise the manufacturer of "+data_rpt[1].lower().split('\n')[0]+" "+data_rpt[3]+' \nfrom\n\"'+data_rpt[2].split('\n')[0]+'\"\nto\n\"'+data[2].split('\n')[0]+'\".'
+    elif cmd=="RE":
+        sentence='Revise the model name of '+data_rpt[1].lower().split('\n')[0]+" by "+data_rpt[2].split('\n')[0]+'\nfrom\n\"'+data_rpt[3]+'\"\nto\n\"'+data[3]+'\".'
+    elif cmd=="RF":
+        sentence="Revise the technical data of "+data_rpt[1].lower().split('\n')[0]+" "+data_rpt[3]+" by "+data_rpt[2].split('\n')[0]+"\nfrom\n\""+data_rpt[4]+"\"\nto\n\""+data[4]+"\"."
+    elif cmd=="A":
+        sentence='Add alternative '+data[1].lower().split('\n')[0]+' '+data[3]+' by '+data[2].split('\n')[0]
+    sheet12[f'c{row}'].value='4'
+    sheet12[f'd{row}'].value=data_rpt[0]
+    sheet12[f'e{row}'].value=sentence
 #    wb.save('output1.xls')
         
     
