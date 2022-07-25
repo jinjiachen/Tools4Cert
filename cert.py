f8e6bffcf05ee351c67166112fe37f932995e901
jinjiachen <jinjiachen_tux@163.com>
2022-07-25 23:04:46 +0800

    add some new function

--- cert.py
+++ cert.py
@@ -9,7 +9,7 @@ from lxml import etree
 options = webdriver.ChromeOptions()
 options.add_argument("--proxy-server=http://192.168.2.108:8889")
 #options.add_argument("--no-proxy-server")
-options.add_argument("--headless")
+#options.add_argument("--headless")
 #options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"')
 #options.add_argument('user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"')
 #options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36')
@@ -26,7 +26,7 @@ driver=webdriver.Chrome(chrome_options=options)
 
 
 print('start already')
-driver.get('https://iq.ulprospector.com/en/')
+driver.get('https://iq.ulprospector.com/en')
 
 #user=driver.find_element_by_id('email')#老方法，不推荐使用
 #password=driver.find_element_by_id('loginPass')#老方法，不推荐使用
@@ -46,7 +46,8 @@ print('login')
 
 #方法二：模拟点击来进行访问
 search=driver.find_element(By.ID,'q2')
-search.send_keys('E231292')
+#search.send_keys('E231292')
+search.send_keys('s7312')
 search.submit()
 
 #print(driver.find_element(By.XPATH,'//td[@class="entry"]').text)
@@ -57,11 +58,27 @@ selector=etree.HTML(html)
 #print(selector)
 company=selector.xpath('//tr[@class=" "]/td[2]/div/span/text()')
 Control=selector.xpath('//td[@class="entry"]/a/span/text()')
+base_url='https://iq.ulprospector.com'
+link=selector.xpath('//td[@class="entry"]/a/@href')
 CCN=selector.xpath('//tr[@class=" "]/td[4]/div/span/text()')
 #txt=selector.xpath('//span/text()')
 print(company)
 print(Control)
 print(CCN)
+print(base_url+link[0])
+
+driver.get(base_url+link[2])
+time.sleep(1)
+html=driver.page_source
+print(html)
+selector=etree.HTML(html)
+name=selector.xpath('//nameline/text()')
+addressline=selector.xpath('//addressline/text()')
+print('\n')
+print(name)
+print(addressline)
+
+
 #cookies=driver.get_cookies()
 #url=driver.current_url()
 #print(driver.page_source)
