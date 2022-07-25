25d055a9a471fdb5692dee986efefbc4790437ca
jinjiachen <jinjiachen_tux@163.com>
2022-07-25 21:59:17 +0800

    optimize the function of returning page source

--- cert.py
+++ cert.py
@@ -9,7 +9,7 @@ from lxml import etree
 options = webdriver.ChromeOptions()
 options.add_argument("--proxy-server=http://192.168.2.108:8889")
 #options.add_argument("--no-proxy-server")
-#options.add_argument("--headless")
+options.add_argument("--headless")
 #options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"')
 #options.add_argument('user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"')
 #options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36')
@@ -25,32 +25,43 @@ driver=webdriver.Chrome(chrome_options=options)
 #driver.set_script_timeout(10)
 
 
-time.sleep(3)
 print('start already')
-#driver.get('https://www.baidu.com')
 driver.get('https://iq.ulprospector.com/en/')
-#user=driver.find_element_by_id('email')
+
+#user=driver.find_element_by_id('email')#老方法，不推荐使用
+#password=driver.find_element_by_id('loginPass')#老方法，不推荐使用
+#login=driver.find_element_by_id('main-login') #老方法，不推荐使用
+#search=driver.find_element_by_id('q2') #老方法，不推荐使用
+
 user=driver.find_element(By.ID,'email')
 user.send_keys('shelway.wu@intertek.com')
-#password=driver.find_element_by_id('loginPass')
 password=driver.find_element(By.ID,'loginPass')
 password.send_keys('Ul123456')
-#login=driver.find_element_by_id('main-login')
 login=driver.find_element(By.ID,'main-login')
 login.submit()
 print('login')
-#search=driver.find_element_by_id('q2')
+
+#方法一：因为是get方法，直接网址来进行访问
+#driver.get('https://iq.ulprospector.com/en/_?p=10005,10048,10006,10047&qm=q:E231292')
+
+#方法二：模拟点击来进行访问
 search=driver.find_element(By.ID,'q2')
 search.send_keys('E231292')
 search.submit()
+
 #print(driver.find_element(By.XPATH,'//td[@class="entry"]').text)
+time.sleep(1)
 html=driver.page_source
+#print(html)
 selector=etree.HTML(html)
+#print(selector)
 txt=selector.xpath('//tr[@class=" "]/td[2]/div/span/text()')
+#txt=selector.xpath('//span/text()')
+print(txt)
 #cookies=driver.get_cookies()
 #url=driver.current_url()
-print(txt)
 #print(driver.page_source)
+
 #while True:
 #    E=input('Please input the control number:')
 #    if E=='exit':
@@ -58,8 +69,7 @@ print(txt)
 #    else:
 #        driver.get('https://iq.ulprospector.com/en/_?p=10005,10048,10006,10047&qm=q:'+E)
 #        print('done')
-#driver.get('https://www.ul.com')
-#driver.get('https://www.taobao.com')
+
 time.sleep(10)
 driver.close()
 driver.quit()
