1b5e830bc8509c3900e9e0ed235476ea70490188
jinjiachen_tux@163.com <jinjiachen_tux@163.com>
2024-07-08 17:23:45 +0800

    增加不同的信息输出方式

--- Equipment_List_new.py
+++ Equipment_List_new.py
@@ -23,17 +23,26 @@ def read_excel():
     if len(res)==0:
         print('无此设备！')
     else:
-        #提取需要的信息
-        name=res['EQUIPMENT NAME'].values[0]
-        PL=res['PL'].values[0]
-        location=res['location'].values[0]
-#        cal_start=res['CAL.DATE'].values[0]
-#        cal_end=res['CAL.NEXT DUE'].values[0]
-        status=res['STATUS'].values[0]
-        print('#'*20)
-#        print(f'设备号：{EC_num}\n设备名称：{name}\n地址：{PL}: {location}\n计量日期：{cal_start} to {cal_end}\n状态：{status}')
-        print(f'设备号：{EC_num}\n设备名称：{name}\n地址：{PL}: {location}\n状态：{status}')
-        print('#'*20+'\n')
+        try:
+            #提取需要的信息
+            name=res['EQUIPMENT NAME'].values[0]
+            PL=res['PL'].values[0]
+            location=res['location'].values[0]
+            cal_start=res['CAL.DATE'].values[0]
+            cal_end=res['CAL.NEXT DUE'].values[0]
+            status=res['STATUS'].values[0]
+            print('#'*20)
+            print(f'设备号：{EC_num}\n设备名称：{name}\n地址：{PL}: {location}\n计量日期：{cal_start} to {cal_end}\n状态：{status}')
+            print('#'*20+'\n')
+        except:
+            #提取需要的信息
+            name=res['EQUIPMENT NAME'].values[0]
+            PL=res['PL'].values[0]
+            location=res['location'].values[0]
+            status=res['STATUS'].values[0]
+            print('#'*20)
+            print(f'设备号：{EC_num}\n设备名称：{name}\n地址：{PL}: {location}\n状态：{status}')
+            print('#'*20+'\n')
 
 
 if __name__=='__main__':
